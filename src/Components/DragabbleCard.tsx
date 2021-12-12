a1186b0163501f4f0cf35a454bcb72c0cc66e653
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-12-12 23:35:58 +0900

    Performance

---
+++ src/Components/DragabbleCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  background-color: ${(props) => props.theme.cardColor};
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  console.log("go");
+
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DragabbleCard);
